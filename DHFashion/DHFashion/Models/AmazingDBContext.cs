9a5310cbe0758f38ac8e09a6e4bfc1890ecc4d24
dhoang2101 <dhoang2101@gmail.com>
2020-03-28 16:33:20 +0700

    new project

---
+++ DHFashion/DHFashion/Models/AmazingDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DHFashion.Models
+{
+    public class AmazingDBContext: DbContext
+    {
+        public AmazingDBContext(DbContextOptions<AmazingDBContext> options):base(options)
+        {
+        }
+        public DbSet<Items> Items { get; set; }
+    }
+}
---
+++ DHFashion/DHFashion/Models/Items.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DHFashion.Models
+{
+    public class Items
+    {
+        [Key]
+        public int Id { get; set; }
+        public String ClothesName { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
